001b6c7b85ecfc3f0b1f6e330bc09ee118c98a39
GAKU <gakugaku1114@iCloud.com>
2023-10-09 06:04:44 +0000

    Update 3 files

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,28 @@
 import "./styles.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
 
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+
+  //削除ボタンのタグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  console.log(deleteButton);
+
+  div.appendChild(li);
+
+  const incompleteList = document.getElementById("incomplete-list");
+  incompleteList.appendChild(div); // ここでエラーが発生していた部分を修正
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
